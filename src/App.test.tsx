4093303a41032867592fbe1dd54e12a6a58ec697
nilaster <nilaster@riseup.net>
2024-05-10 11:58:57 +0200

    test: update test units

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,26 @@
-import React from "react"
-import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
-import { App } from "./App"
+import React from "react";
+import { screen } from "@testing-library/react";
+import { render } from "./test-utils";
+import { App } from "./App";
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
-})
+test("renders header", () => {
+  render(<App />);
+  const headerElement = screen.getByText(/best task manager in the world/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test("renders create new task button", () => {
+  render(<App />);
+  const addTaskButton = screen.getByText(/create new task/i);
+  expect(addTaskButton).toBeInTheDocument();
+});
+
+test("renders columns", () => {
+  render(<App />);
+  const notStartedColumn = screen.getByText(/not started/i);
+  const inProgressColumn = screen.getByText(/in progress/i);
+  const doneColumn = screen.getByText(/done/i);
+  expect(notStartedColumn).toBeInTheDocument();
+  expect(inProgressColumn).toBeInTheDocument();
+  expect(doneColumn).toBeInTheDocument();
+});
